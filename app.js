b007d6c155400c2ebaeb0b91e93fdc304a73c382
vnkvp <vnkvp@outlook.com>
2019-06-10 11:24:28 -0300

    mysqlinstalled

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var app = require('./config/server')
-app.get('/', function (req, res) {
-    res.render('home/index');
-});
-app.get('/form-add-noticia', function (req, res) {
-    res.render('admin/form_add_noticia');
-});
-app.get('/noticias', function (req, res) {
-    res.render('noticias/noticias');
-});
+var app = require('./config/server');
+
+var rotaHome = require('./app/routes/home');
+rotaHome(app);
+var rotaFormAddNoticias = require('./app/routes/form-add-noticias');
+rotaFormAddNoticias(app);
+var rotaNoticias = require('./app/routes/noticias');
+rotaNoticias(app);
+
 app.listen(3000, function () {
     console.log('server ON');
 });
